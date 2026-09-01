8468e9495998ee8c37c478c66161cf5d8d54b9e6
kakaria <kariadj4ng0@gmail.com>
2026-09-01 09:40:26 +0700

    refactor(core): remove redudant comment

--- core/thread_local.py
+++ core/thread_local.py
@@ -1,21 +1,3 @@
-# import threading
-
-# _thread_local = threading.local()
-
-
-# def set_current_tenant(tenant_id):
-#     _thread_local.tenant_id = tenant_id
-
-
-# def get_current_tenant():
-#     return getattr(_thread_local, "tenant_id", None)
-
-
-# def clear_thread_local():
-#     if hasattr(_thread_local, "tenant_id"):
-#         del _thread_local.tenant_id
-
-
 import threading
 
 _thread_local = threading.local()
